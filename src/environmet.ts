498c4632c33784f910005930737cbef07c491478
facundo000 <facundo.guzman@mi.unc.edu.ar>
2025-01-21 14:33:40 -0300

    ocultar variables

--- src/environmet.ts
+++ src/environmet.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://api-backend-bienesraices-nest.onrender.com',    
-    urlImg: 'https://api-backend-bienesraices-nest.onrender.com/api/v1/files/propiedad/',
+    apiUrl: 'https://api',    
+    urlImg: 'https://api-imagenes',
   };
   
